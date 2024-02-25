71412c8a843b9c48f3fdd38b38e6816e427eb816
D Gartland <dgartland021@c2ken.net>
2024-02-25 10:31:12 +0000

    Initial commit of website files

---
+++ _vti_cnf/Text Sizer.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|17 Nov 2023 14:50:03 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|C2KEN\\jfarthing070
+vti_modifiedby:SR|C2KEN\\jfarthing070
+vti_timecreated:TR|17 Nov 2023 14:50:03 -0000
+vti_cacheddtm:TX|17 Nov 2023 14:50:03 -0000
+vti_filesize:IR|1270
+vti_backlinkinfo:VX|
